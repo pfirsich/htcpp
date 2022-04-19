fc1c46682f361ecef08655d0d8b49a650ff5c8db
Joel Schumacher <joelschum@gmail.com>
2022-04-20 00:46:37 +0200

    Use SOMAXCONN by default for listen backlog size

--- src/config.hpp
+++ src/config.hpp
@@ -3,7 +3,7 @@
 struct Config {
     uint16_t listenPort = 6969;
     uint32_t listenAddress = INADDR_ANY;
-    size_t listenBacklog = 1024;
+    size_t listenBacklog = SOMAXCONN;
     size_t ioQueueSize = 2048; // power of two, >= 1, <= 4096
     size_t readAmount = 128;
     size_t singleReadTimeoutMs = 512; // todo
