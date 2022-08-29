d7c287954d668d1df8e5c0a3855ec56e83d37889
Joel Schumacher <joelschum@gmail.com>
2022-08-29 20:22:45 +0200

    Add a bridge to make HTTP requests from a different thread
    I don't know how much I like this idea right now, but it's enough for
    what I want to do and it's not too horrible, I think. It couples with
    nothing (stricly on top of existing code) and has the interface I want,
    so I might keep it even if it is not pretty.

--- src/clienttest.cpp
+++ src/clienttest.cpp
@@ -1,10 +1,57 @@
 #include <cpprom/cpprom.hpp>
 
 #include "client.hpp"
+#include "events.hpp"
 
 #ifdef TLS_SUPPORT_ENABLED
 #include "ssl.hpp"
 #endif
+
+#include <future>
+
+class ThreadSafeRequester {
+public:
+    struct RequestResult {
+        std::error_code ec;
+        Response response;
+    };
+
+    ThreadSafeRequester(IoQueue& io)
+        : io_(io)
+        , eventListener_(io, [this](Event&& event) { eventHandler(std::move(event)); })
+    {
+    }
+
+    std::future<RequestResult> request(
+        Method method, std::string url, HeaderMap<> headers = {}, std::string body = {})
+    {
+        auto prom = std::make_shared<std::promise<RequestResult>>();
+        auto fut = prom->get_future();
+        eventListener_.emit(
+            Event { method, std::move(url), std::move(headers), std::move(body), std::move(prom) });
+        return fut;
+    }
+
+private:
+    struct Event {
+        Method method;
+        std::string url;
+        HeaderMap<> headers;
+        std::string body;
+        std::shared_ptr<std::promise<RequestResult>> promise;
+    };
+
+    void eventHandler(Event&& event)
+    {
+        ::request(io_, event.method, event.url, event.headers, event.body,
+            [prom = std::move(event.promise)](std::error_code ec, Response&& resp) mutable {
+                prom->set_value(RequestResult { ec, std::move(resp) });
+            });
+    }
+
+    IoQueue& io_;
+    EventListener<Event> eventListener_;
+};
 
 int main()
 {
@@ -12,19 +59,22 @@ int main()
 
     IoQueue io;
 
-    request(io, Method::Get, "https://httpbin.org/get", {}, "",
-        [](std::error_code ec, Response&& resp) {
-            if (ec) {
-                slog::error("Error in request: ", ec.message());
-                return;
-            }
+    ThreadSafeRequester requester(io);
 
-            slog::info("Status: ", static_cast<uint32_t>(resp.status));
-            for (const auto& [name, value] : resp.headers.getEntries()) {
-                slog::info("'", name, "': '", value, "'");
-            }
-            slog::info("Body:\n", resp.body);
-        });
+    std::thread t([&requester]() {
+        const auto [ec, resp] = requester.request(Method::Get, "http://httpbin.org/get").get();
+
+        if (ec) {
+            slog::error("Error in request: ", ec.message());
+            return;
+        }
+
+        slog::info("Status: ", static_cast<uint32_t>(resp.status));
+        for (const auto& [name, value] : resp.headers.getEntries()) {
+            slog::info("'", name, "': '", value, "'");
+        }
+        slog::info("Body:\n", resp.body);
+    });
 
     io.run();
 
