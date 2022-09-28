6fc0637fd23a29b4fdc545963724e1cfc8f7b75a
Joel Schumacher <joelschum@gmail.com>
2022-09-28 17:36:21 +0200

    Move some code to client.cpp

--- src/client.hpp
+++ src/client.hpp
@@ -304,26 +304,6 @@ private:
     std::unique_ptr<Connection> connection_;
 };
 
+// This must be called from the main thread! You can tell by the IoQueue& parameter
 void request(IoQueue& io, Method method, const std::string_view urlStr, const HeaderMap<>& headers,
-    const std::string& requestBody, std::function<void(std::error_code, Response&&)> cb)
-{
-    const auto url = Url::parse(urlStr);
-    if (!url) {
-        slog::error("Could not parse URL for request");
-        cb(std::make_error_code(std::errc::invalid_argument), Response());
-        return;
-    }
-    if (url->scheme == "http") {
-        auto session = ClientSession<TcpConnectionFactory>::create(io, url->host, url->port);
-        session->request(method, url->targetRaw, headers, requestBody, std::move(cb));
-#ifdef TLS_SUPPORT_ENABLED
-    } else if (url->scheme == "https") {
-        auto session = ClientSession<SslClientConnectionFactory>::create(io, url->host, url->port);
-        session->request(method, url->targetRaw, headers, requestBody, std::move(cb));
-#endif
-    } else {
-        cb(std::make_error_code(std::errc::invalid_argument), Response());
-        slog::error("Invalid scheme in request url");
-        return;
-    }
-}
+    const std::string& requestBody, std::function<void(std::error_code, Response&&)> cb);
